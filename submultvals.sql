269ed37c63c65e817d993505a68896a79f4f6e4f
Jordan Godwin <jordan.godwin13@gmail.com>
2023-05-16 13:51:56 -0500

    Add query with multiple return values in subquery

---
+++ submultvals.sql
@@ -0,0 +1,23 @@
+/*
+CREATED BY: JORDAN GODWIN
+CREATE DATE: 05/16/2023
+DESCRIPTION: RETURNING MULTIPLE VALUES FROM SUBQUERY
+*/
+
+SELECT
+	InvoiceDate,
+	BillingAddress,
+	BillingCity
+
+FROM
+	Invoice
+	
+WHERE
+	InvoiceDate IN
+
+(SELECT
+	InvoiceDate
+FROM
+	Invoice
+WHERE
+	InvoiceId IN (251, 252, 254))
